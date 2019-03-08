4582030d4dcf915494ca862fb788684e391301e7
Christopher Koerber <christopher@ckoerber.com>
2019-03-08 15:21:25 -0800

    New export script for fitting ground state only again

--- scripts/export-energies.py
+++ scripts/export-energies.py
@@ -55,6 +55,16 @@ class Minimizer:  # pylint: disable=R0903
 
         return np.sum(np.array([y[0], y_intp(0) * weight_at_zero, y[1]]) ** 2)
 
+    def cost_function_x0(self, c0: float) -> float:
+        r"""Computes $p \\cot(delta(p))$ at first energy level and sets this guy to zero.
+        """
+        ground_state = self.solver.get_energies(c0, n_energies=1)[0]
+        x = ground_state * self.mu * self.L ** 2 / 2 / np.pi ** 2
+        if not x < 0:
+            raise ValueError("Smallest energy not a bound state")
+
+        return self.zeta3d(x) ** 2
+
 
 def main(L: int = 1.0):  # pylint: disable=R0914
     """Compute energy levels for different derivative implementations
@@ -71,12 +81,15 @@ def main(L: int = 1.0):  # pylint: disable=R0914
             columns=["epsilon", "n1d_max", "nstep", "c", "nlevel", "energy"]
         )
 
-    for nstep in range(1, 5):
-        for epsilon in epsilons:
+    for epsilon in epsilons:
+        initial = -1.0
+        for nstep in range(1, 5):
+
             print(f"[+] nstep = {nstep}, epsilon = {epsilon}")
 
-            if df.query("epsilon == @epsilon and nstep == @nstep").empty:
+            if not df.query("epsilon == @epsilon and nstep == @nstep").empty:
                 print("[+] Skip")
+                continue
 
             n1d_max = int(L / epsilon)
             L_eff = n1d_max * epsilon
@@ -89,9 +102,15 @@ def main(L: int = 1.0):  # pylint: disable=R0914
             )
 
             minimizer = Minimizer(solver, L_eff, epsilon, mu)
-            res = opt.minimize(minimizer, -1.0, bounds=((-4.0, -1.0e-4),), method="TNC")
+            res = opt.minimize(
+                minimizer.cost_function_x0,
+                initial,
+                bounds=((-4.0, -1.0e-4),),
+                method="TNC",
+            )
 
             energies = solver.get_energies(res.x, n_energies=min((n1d_max - 2) ** 2, 60))
+            initial = res.x[0]
 
             for nlevel, energy in enumerate(energies):
                 data.append(
@@ -106,7 +125,7 @@ def main(L: int = 1.0):  # pylint: disable=R0914
                 )
 
             tf = pd.DataFrame(data)
-            df = df.append(tf)
+            df = df.append(tf, sort=False)
             df.to_csv("luescher-3d-res.csv", index=False)
 
 
