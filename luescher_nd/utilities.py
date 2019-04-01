4023c48f0de6833b394766be8053bff3ac9c9067
Christopher Koerber <christopher@ckoerber.com>
2019-04-01 15:25:43 -0700

    Added analytic laplace coefficients option

--- luescher_nd/utilities.py
+++ luescher_nd/utilities.py
@@ -13,9 +13,12 @@ from typing import Dict
 import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as lina
+from scipy.sparse.linalg import LinearOperator
 from scipy.special import gamma as gammafunc
 
-from scipy.sparse.linalg import LinearOperator
+from sympy import Matrix
+from sympy.functions.combinatorial.factorials import factorial as analytic_factorial
+
 
 try:
     from solvers.src import cupy_sp
@@ -47,23 +50,36 @@ def factorial(n: np.ndarray) -> np.ndarray:  # pylint: disable=C0103
     return gammafunc(n + 1)
 
 
-def get_laplace_coefficients(n_step: int):
+def get_laplace_coefficients(n_step: int, analytic: bool = False):
     """Computes the derivative coefficient for lapace operator up to step range Nstep.
 
     The dispersion of the related momentum squared operator is equal to `p ** 2` up to
     order `p ** (2 * NStep)`.
 
     **Arguments**
-        Nstep: int
+        n_step: int
             The number of momentum steps in each direction.
+
+        analytic: bool = False
+            Solves analytically for coefficients and converts result to floats
+            afterwards. Takes more time to compute coefficients.
     """
-    v = np.zeros(n_step + 1)
+    v = np.zeros(n_step + 1, dtype=int)
     v[1] = 1
 
-    nn, mm = np.meshgrid(*[np.arange(n_step + 1)] * 2)
-    A = 1 / factorial(2 * mm) * (-1) ** mm * nn ** (2 * mm)
+    if analytic:
+        A = Matrix([[0] * v.size] * v.size)
+        v = Matrix(v)
+        for n, m in itertools.product(*[range(n_step + 1)] * 2):
+            A[m, n] = 1 / analytic_factorial(2 * m) * (-1) ** m * n ** (2 * m)
 
-    gamma = np.linalg.solve(A, v)
+        gamma = np.array(A.LUsolve(v)).flatten().astype(float)
+
+    else:
+        nn, mm = np.meshgrid(*[np.arange(n_step + 1)] * 2)
+        A = 1 / factorial(2 * mm) * (-1) ** mm * nn ** (2 * mm)
+
+        gamma = np.linalg.solve(A, v)
 
     coeffs = {}
     for n, coeff in enumerate(gamma):
