75496cee687a43190a5d0077d7ca5800b0b0834b
Christopher Koerber <christopher@ckoerber.com>
2019-04-12 12:24:39 -0700

    Removed typecheck for zeta arg

--- luescher_nd/zeta/extern/pyzeta.pyx
+++ luescher_nd/zeta/extern/pyzeta.pyx
@@ -5,8 +5,8 @@ import numpy as np
 cdef extern from "zeta.cc":
     double zetaRGL_re(int l, int m, const vector[double] s, double gam, double usq)
 
-def zeta(const vector[double] x):
+def zeta(x):
     """Zeta function for m=0, l=0 channel with zero CMS momentum
     """
-    x = x if hasattr(x, "__iter__") else [x] 
+    x = x if hasattr(x, "__iter__") else [x]
     return np.array([zetaRGL_re(0, 0, [0.0, 0.0, 0.0], 1.0, xi) for xi in x])
