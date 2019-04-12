6e4703304817ddbaf019ea16a727050a5b3844c8
Christopher Koerber <christopher@ckoerber.com>
2019-04-12 16:11:41 -0700

    Added plotting scripts

--- luescher_nd/plotting/export.py
+++ luescher_nd/plotting/export.py
@@ -1,4 +1,4 @@
-"""
+"""Contains plot functions for effective range expansion data
 """
 import os
 
@@ -9,49 +9,74 @@ from luescher_nd.zeta.extern import pyzeta as pyz
 from luescher_nd.zeta.zeta3d import Zeta3D
 
 from luescher_nd.plotting.utilities import plot_ere_grid
+from luescher_nd.plotting.utilities import raw_data_to_plot_frame
+from luescher_nd.plotting.utilities import monotonous_bounds
 
 
-DATA = os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir, "data"))
+DATA = os.path.abspath(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), os.pardir, os.pardir, "data"
+    )
+)
 
 
-def dispersion_luescher_grid(fout: str):
+def dispersion_luescher_grid(fout: str):  # pylint: disable=R0914
+    """Plots the effective range expansion for dispersion Lüscher data.
+
+    Creates a grid plot and saves figure to `fout`.
     """
-    """
-    fin = os.path.join(DATA, "dispersion-luescher-res.csv")
-    df = pd.read_csv(fin)
+    fin = os.path.join(DATA, "luescher-3d-dispersion-final.csv")
+    df = raw_data_to_plot_frame(
+        pd.read_csv(fin, dtype={"nstep": "Int64"}), dispersion_zeta=True
+    )
 
     grid = plot_ere_grid(df)
 
-    x = np.linspace(-4, int(df.x.max()) + 1)
+    x = np.linspace(-4, int(df.x.max()) + 1, 50000)
+    L = df.L.round(7).unique()[0]
 
-    for ax in np.array(grid.axes).flatten():
-        L, epsilon, nstep = None, None, None
+    nstep_map = {r"$\infty$": None}
+    nstep_map.update({f"${nstep}$": nstep for nstep in df.nstep.unique() if nstep > 0})
+    for (nrow, ncol, nhue), _ in grid.facet_data():
+        n1d_max = grid.hue_names[nhue]
+        nstep = nstep_map[grid.col_names[ncol]]
+        if n1d_max != 20:
+            continue
+        epsilon = L / n1d_max
         zeta = Zeta3D(L, epsilon, nstep)(x)
-        ax.plot(x, zeta, ls=":", lw=0.2)
+        for imin, imax in monotonous_bounds(zeta):
+            grid.axes[nrow, ncol].plot(
+                x[imin:imax], zeta[imin:imax], ls=":", lw=0.5, c="black", zorder=-1
+            )
 
     grid.fig.set_dpi(300)
-    grid.fig.set_figheight(6)
+    grid.fig.set_figheight(2)
     grid.fig.set_figwidth(6)
 
     grid.fig.savefig(fout, bbox_inches="tight")
 
 
 def standard_luescher_grid(fout: str):
+    """Plots the effective range expansion for standard Lüscher data.
+
+    Creates a grid plot and saves figure to `fout`.
     """
-    """
-    fin = os.path.join(DATA, "standard-luescher-res.csv")
-    df = pd.read_csv(fin)
+    fin = os.path.join(DATA, "luescher-3d-standard-final.csv")
+    df = raw_data_to_plot_frame(
+        pd.read_csv(fin, dtype={"nstep": "Int64"}), dispersion_zeta=False
+    )
 
     grid = plot_ere_grid(df)
 
-    x = np.linspace(-4, int(df.x.max()) + 1)
-    zeta = pyz.zeta(x)
+    x = np.linspace(-4, int(df.x.max()) + 1, 1000)
+    zeta = pyz.zeta(x)  # pylint: disable=I1101
 
     for ax in np.array(grid.axes).flatten():
-        ax.plot(x, zeta, ls=":", lw=0.2)
+        for imin, imax in monotonous_bounds(zeta):
+            ax.plot(x[imin:imax], zeta[imin:imax], ls=":", lw=0.5, c="black", zorder=-1)
 
     grid.fig.set_dpi(300)
-    grid.fig.set_figheight(6)
+    grid.fig.set_figheight(2)
     grid.fig.set_figwidth(6)
 
     grid.fig.savefig(fout, bbox_inches="tight")
--- luescher_nd/plotting/utilities.py
+++ luescher_nd/plotting/utilities.py
@@ -1,34 +1,121 @@
 """Plotting utility functions for luescher_nd project
 """
+from typing import List
+from typing import Tuple
+
+from itertools import product
+
 import pandas as pd
 import numpy as np
 
 import matplotlib.pylab as plt
 import seaborn as sns
 
+from luescher_nd.zeta.zeta3d import Zeta3D
+from luescher_nd.zeta.extern import pyzeta
 
-def plot_ere_grid(df: pd.DataFrame, x="x", y="y", **kwargs) -> sns.FacetGrid:
+## Parameters & Files
+HBARC = 197.326  # MeV / fm
+M_NUCLEON = (938.27 + 939.57) / 2 / HBARC  # in fm^{-1}
+
+MU = M_NUCLEON / 2
+
+
+def plot_ere_grid(  # pylint: disable=C0103
+    df: pd.DataFrame, x="x", y="y", **kwargs
+) -> sns.FacetGrid:
     """Plots effective range expansion grid for input DataFrame.
     """
     default_kwargs = {
-        "col": "nstep",
+        "col": "$n_s$",
         "hue": "n1d_max",
         "hue_kws": {"marker": ["s", "o", "v", "d"]},
         "palette": "cubehelix",
         "margin_titles": True,
+        "ylim": (-2, 2),
+        "xlim": (-1, 10),
     }
+    default_kwargs["col_order"] = sorted(df[default_kwargs["col"]].unique())
 
     default_kwargs.update(**kwargs)
 
     g = sns.FacetGrid(df, **default_kwargs)
-    g.map(plt.plot, x, y, ms=1, lw=0.5, ls="--")
+    g.map(plt.plot, x, y, ms=2, lw=1, ls="--")
 
     for ax in np.array(g.axes).flatten():
-        ax.axhline(0, c="black", lw=0.2)
-        ax.axvline(0, c="black", lw=0.2)
+        ax.axhline(0, c="grey", lw=1, zorder=-2)
 
     g.set_xlabels("$x$")
     g.set_ylabels(r"$p \cot (\delta_0(p))$")
     g.add_legend(title=r"$\frac{L}{\epsilon}$", fontsize=6)
 
     return g
+
+
+def monotonous_bounds(vals: np.ndarray) -> List[Tuple[int, int]]:
+    """Returns indices pairs of array such that each split is monotously increasing.
+    """
+    bounds = []
+    prev_val = vals[0]
+    prev_bound = 0
+    for ind, val in enumerate(vals[1:]):
+        if val < prev_val:
+            bounds += [(prev_bound, ind + 1)]
+            prev_bound = ind + 1
+        prev_val = val
+    bounds += [(prev_bound, len(vals))]
+
+    return bounds
+
+
+def raw_data_to_plot_frame(  # pylint: disable=C0103
+    df: pd.DataFrame, dispersion_zeta: bool = True, ycut: float = 2.0
+) -> pd.DataFrame:
+    """Converts a raw data data frame to a plotting data frame.
+
+    Adds columns and computes results of zeta function.
+
+    **Arguments**
+        df: pd.DataFrame
+            Raw data input frame.
+
+        dispersion_zeta: bool = True
+            Use dispersion zeta function or standard zeta function.
+
+        ycut: float = 2.0
+            Drop data which has larger |y| than ycut.
+    """
+    zeta = {}
+
+    df["L"] = df["n1d_max"] * df["epsilon"]
+    df["x"] = df["energy"] * MU * df["L"] ** 2 / 2 / np.pi ** 2
+    df.loc[df.nstep.isna(), "nstep"] = -1
+
+    for epsilon, L, nstep in product(
+        df.epsilon.unique(), df.L.unique(), df.nstep.unique()
+    ):
+        zeta[(epsilon, L, nstep)] = (
+            Zeta3D(L, epsilon, nstep=(None if nstep < 0 else nstep))
+            if dispersion_zeta
+            else pyzeta.zeta  # pylint: disable=I1101
+        )
+
+    df["y"] = df.apply(
+        lambda row: zeta[(row["epsilon"], row["L"], row["nstep"])](row["x"])[0]
+        / np.pi
+        / row["L"],
+        axis=1,
+    )
+    df.loc[df["y"].abs() > ycut, "y"] = np.nan
+    df["$n_s$"] = df.apply(
+        lambda row: r"$\infty$" if row["nstep"] < 0 else f"${row['nstep']}$", axis=1
+    )
+
+    df = (
+        df.drop_duplicates(["epsilon", "nstep", "y"])
+        .dropna()
+        .reset_index(drop=True)
+        .sort_values(["n1d_max", "nstep", "x", "nlevel"], ascending=True)
+    )
+
+    return df
