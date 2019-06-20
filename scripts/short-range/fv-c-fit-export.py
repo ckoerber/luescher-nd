4becc8c797978b7da23c1b3eef8326816c9237cc
Christopher Koerber <christopher@ckoerber.com>
2019-06-20 09:13:40 -0700

    Added databases and export scripts for finite scattering

--- scripts/short-range/fv-c-fit-export.py
+++ scripts/short-range/fv-c-fit-export.py
@@ -17,13 +17,16 @@ from scipy.sparse.linalg import eigsh
 from scipy.optimize import minimize_scalar
 
 from luescher_nd.hamiltonians.contact import MomentumContactHamiltonian
+from luescher_nd.operators import get_parity_projector
 
 from luescher_nd.zeta.extern.pyzeta import zeta  # pylint: disable=E0611
 
-RANGES = {"epsilon": [0.05, 0.1, 0.2], "L": [1.0, 2.0], "nstep": [1, 2, 3, 4, None]}
-PARS = {"k": 100}
+RANGES = {"epsilon": [0.020, 0.025, 0.05, 0.1, 0.2, 0.25], "L": [1.0], "nstep": [1]}
+PARS = {"k": 50}
 
-DBNAME = "db-contact-fv-c-fitted.sqlite"
+A_INV = -5.0
+
+DBNAME = "db-contact-fv-c-fitted-parity-a-inv.sqlite"
 
 ROOT = os.path.abspath(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, os.pardir)
@@ -37,6 +40,7 @@ class FitKernel:
     """
 
     h: MomentumContactHamiltonian
+    a_inv: float = 0
     _zeta: Callable[[np.ndarray], np.ndarray] = field(init=False, repr=False)
     _e0: float = field(init=False, repr=False)
 
@@ -53,7 +57,7 @@ class FitKernel:
     def _ere_diff_sqrd(self, x: float) -> float:
         """Computes the difference betewen ERE and zeta function
         """
-        return (0 - self.zeta(x)[0] / np.pi / self.h.L) ** 2
+        return (self.a_inv - self.zeta(x)[0] / np.pi / self.h.L) ** 2
 
     def _get_ground_state(self) -> float:
         """Computes the first intersection of the zeta function and the ERE
@@ -83,20 +87,34 @@ def main():
     """
     for epsilon, L, nstep in product(RANGES["epsilon"], RANGES["L"], RANGES["nstep"]):
         n1d = int(L / epsilon)
-        h = MomentumContactHamiltonian(n1d=n1d, epsilon=epsilon, nstep=nstep)
+        p_minus = get_parity_projector(n1d, ndim=3, positive=False)
+        h = MomentumContactHamiltonian(
+            n1d=n1d,
+            epsilon=epsilon,
+            nstep=nstep,
+            filter_out=p_minus,
+            filter_cutoff=1.0e2,
+        )
 
-        kernel = FitKernel(h)
+        kernel = FitKernel(h, a_inv=A_INV)
+
         contact_strength = minimize_scalar(
             kernel.chi2, bracket=(-1.0e2, -1.0e-4), options={"xtol": 1.0e-12}
         ).x
 
         MomentumContactHamiltonian(
-            n1d=n1d, epsilon=epsilon, nstep=nstep, contact_strength=contact_strength
+            n1d=n1d,
+            epsilon=epsilon,
+            nstep=nstep,
+            contact_strength=contact_strength,
+            filter_out=p_minus,
+            filter_cutoff=1.0e2,
         ).export_eigs(
             DB,
             eigsh_kwargs=PARS,
             export_kwargs={
-                "comment": "potential fitted to Finite Volume discrete ground state"
+                "comment": "potential fitted to Finite Volume"
+                " discrete ground state (a_inv={A_INV})"
             },
         )
 
