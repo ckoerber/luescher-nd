a301886a8a837e0188fbe257751b904cde1554e5
Christopher Koerber <christopher@ckoerber.com>
2019-07-10 16:29:52 -0700

    Added functionality to extrapolate spectrum to zero.

--- luescher_nd/database/utilities.py
+++ luescher_nd/database/utilities.py
@@ -1,11 +1,16 @@
 """Utility functions for database submodule
 """
+from typing import Dict
+
 from itertools import product
 
 import os
 
 import numpy as np
 import pandas as pd
+
+import lsqfit
+import gvar as gv
 
 from luescher_nd.database.connection import DatabaseSession
 
@@ -18,8 +23,10 @@ DATA_FOLDER = os.path.abspath(
     os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "data")
 )
 
+np.random.seed(42)
 
-def read_table(
+
+def read_table(  # pylint: disable=R0913, R0914
     database: str,
     round_digits: int = 2,
     dispersion_zeta: bool = True,
@@ -110,3 +117,128 @@ def read_table(
     df["nstep"] = df["nstep"].astype(str)
 
     return df
+
+
+def _even_poly(epsilon: np.ndarray, p: Dict[str, gv.GVar]):  # pylint: disable=E1101
+    """Even polynomial used for fit.
+
+    `y = sum(x_n epsilon**(2n), n=0, n_max)`.
+
+    **Arguments**
+        epsilon: np.ndarray
+            The lattice spacing to extrapolate.
+
+        p: Dict[str, gv.GVar]
+            The prior. Must contain key "x"
+    """
+    nexp = len(p["x"])
+    exps = np.arange(0, nexp * 2, 2)
+    out = 0
+    for exp, x in zip(exps, p["x"]):
+        out += epsilon ** exp * x
+
+    return out
+
+
+def _group_wise_fit(  # pylint: disable=R0914
+    row: pd.Series, n_poly_max: int = 4, delta_x=1.0e-8, include_statistics: bool = True
+):
+    """Runs a bayesian fit to extrapolate x(epsilon) to zero.
+
+    Requires at least 3 data points, otherwise sets result to None.
+    Prior is smallest x as offset and 10 * random noize.
+
+    Picks the best fit determined by logGBF as the main result.
+
+    **Arguments**
+        row: pd.Series
+            Series containing `epsilon` and `x` columns which are needed for the
+            extrapolation.
+
+        n_poly_max: int = 4
+            Maximal order of the polynomial used for the spectrum extrapolation.
+            The fitter runs fits from 1 to `n_poly_max` even polynomials and picks
+            the best one defined by the maximum of the logGBF.
+
+        delta_x:float=1.0e-8
+            Approximate error for the x-values.
+
+        include_statistics: bool = True
+            Includes fit statistics like chi2/dof or logGBF.
+    """
+    epsilon, x = row["epsilon"].values, row["x"].values
+    error = [delta_x * np.random.uniform(0.5, 2, size=1)[0] for _ in x]
+    data = (epsilon, gv.gvar(x, error))
+
+    if len(x) > 2:
+
+        fits = []
+        log_gbf = []
+        for nexp in range(1, n_poly_max + 1):
+            x_init = np.zeros(nexp)
+            x_init[0] = x[0]
+            x_error = [10 * np.random.uniform(0.5, 2, size=1)[0] for _ in x_init]
+            prior = {"x": gv.gvar(x_init, x_error)}
+
+            fit = lsqfit.nonlinear_fit(data, fcn=_even_poly, prior=prior)
+            fits.append(fit)
+            log_gbf.append(fit.logGBF)
+
+        idx = np.argmax(log_gbf)
+        fit = fits[idx]
+
+        if include_statistics:
+            out = pd.Series(
+                {
+                    "x": fit.p["x"][0].mean,
+                    "sdev": fit.p["x"][0].mean,
+                    "chi2/dof": fit.chi2 / fit.dof,
+                    "logGBF": fit.logGBF,
+                    "n_poly_max": range(1, n_poly_max + 1)[idx],
+                }
+            )
+        else:
+            out = pd.Series({"x": fit.p["x"][0].mean})
+    else:
+        out = None
+
+    return out
+
+
+def get_continuum_extrapolation(  # pylint: disable=C0103
+    df: pd.DataFrame,
+    n_poly_max: int = 4,
+    delta_x: float = 1.0e-8,
+    include_statistics: bool = True,
+) -> pd.DataFrame:
+    """Takes a data frame read in by read tables and runs a continuum extrapolation
+    for the spectrum.
+
+    The continuum extrapolation is executed by a even polynomial fit up to order
+    `n_poly_max`
+
+    **Arguments**
+        df: pd.DataFrame
+            DataFrame returend by `read_table`.
+
+        n_poly_max: int = 4
+            Maximal order of the polynomial used for the spectrum extrapolation.
+            The fitter runs fits from 1 to `n_poly_max` even polynomials and picks
+            the best one defined by the maximum of the logGBF.
+
+        delta_x:float=1.0e-8
+            Approximate error for the x-values.
+
+        include_statistics: bool = True
+            Includes fit statistics like chi2/dof or logGBF.
+    """
+    group = df.groupby(["L", "nstep", "nlevel"])[["epsilon", "x"]]
+    fit_df = group.apply(
+        _group_wise_fit,
+        n_poly_max=n_poly_max,
+        delta_x=delta_x,
+        include_statistics=include_statistics,
+    ).reset_index()
+    fit_df["epsilon"] = 0
+
+    return fit_df
