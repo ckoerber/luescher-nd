fcace57860f421ccd9c87ac70a55f7eac4d41a61
Evan Berkowitz <git@evanberkowitz.com>
2019-07-11 16:45:29 +0200

    Reducer.

--- luescher_nd/operators.py
+++ luescher_nd/operators.py
@@ -135,6 +135,8 @@ def get_A1g_operator(n1d: int, ndim: int, axis=0) -> sp.csr_matrix:
     # print(nsq[peek])
     # print(len(nsq[peek]))
     for n2 in nsq:
+        # print(f"------------{n2}------------")
+        # print(nsq[n2])
         for i, vi in nsq[n2]:
             # Build a list of guys that meet vi under Oh:
             matched = []
@@ -155,7 +157,7 @@ def get_A1g_operator(n1d: int, ndim: int, axis=0) -> sp.csr_matrix:
             norm = 1/np.sqrt(len(matched))
             for j, vj in matched:
                 A1g_mat[i,j] = norm
-
+        # print(handled)
             # We could build a non-square projector P that we could apply to H, as in P @ H @ Pdagger
             # Written that way, it'd take project to A1g states, dropping all non-A1g from the Hamiltonian.
             # That'd make the Hamiltonian much much much smaller, and therefore (presumably) easier to diagonalize.
@@ -168,4 +170,83 @@ def get_A1g_projector(n1d: int, ndim: int):
     # That will be multiplied by something large, giving non-A1g states a big boost in energy.
     a1g = get_A1g_operator(n1d, ndim)
     one = sp.eye(n1d ** ndim)
-    return one - a1g
+    # return one - a1g
+    return a1g
+
+def get_A1g_reducer(n1d: int, ndim: int, axis=0) -> sp.csr_matrix:
+    """ Implements the projection operator that's schematically | A1g > < A1g |.
+    Because A1g is espeically simple, the procedure works in any dimension, specified by ndim.
+
+    If you have a wavefunction |psi> written as a sum of plane waves, applying this operator will give you
+        sum_{state j that's in A1g} |j><j|psi>
+    If |psi> isn't already in momentum space, who knows what this will do!
+
+    ** Arguments **
+        n1d: int
+            Number of points in one direction.
+        ndim: int
+            Number of spatial dimensions.
+    """
+    ntot = n1d ** ndim
+    A1g_mat = []
+
+    # One-dimensional momenta, with signs.
+    p1d = np.array(list(np.arange(0, (n1d + 1) // 2))
+                    + list(-np.arange(n1d // 2, 0, -1))
+                    )
+
+    # A list of coordinates:
+    momenta = np.transpose([el.flatten() for el in np.meshgrid(*[p1d] * ndim)])
+
+    # Group momenta by norm^2, keeping index
+    nsq = dict()
+    for i,p in enumerate(momenta):
+        n2 = np.dot(p,p)
+        if n2 not in nsq:
+            nsq[n2] = [[i, p]]
+        else:
+            nsq[n2]+=[[i, p]]
+
+    # We can't just take nsq by nsq, because some nsq have more than one
+    # choice for momenta vectors that don't go into one another under octahedral symmetry.
+    # peek=9 # other good choices are 17, 18, 25, ...
+    # print(nsq[peek])
+    # print(len(nsq[peek]))
+    for n2 in nsq:
+        # print(f"------------{n2}------------")
+        # print(nsq[n2])
+        handled = []
+        for i, vi in nsq[n2]:
+            if i in handled:
+                continue
+            # Build a list of guys that meet vi under Oh:
+            matched = []
+            for j, vj in nsq[n2]:
+                if j in handled:
+                    continue
+                match = np.sort(np.abs(vi, dtype=int)) == np.sort(np.abs(vj, dtype=int))
+                if match.all():
+                    matched+=[[j, vj]]
+
+            # With those in hand, we can construct states
+            # It's actually here that we specify A1g at all!
+            # It's A1g because all the entries get the same entry.
+            # A different irrep would require a different pattern.
+            # In principle, we can get these by evaluating Ylm spherical harmonics
+            # on the momentum vectors---you've got to be careful to norm the guys
+            # that live on the boundary of the brillouin zone correctly.
+            # How this works I forget; I just do s-wave / A1g, where the answer is
+            # just always sum and then norm by the number of states.
+            norm = 1/np.sqrt(len(matched))
+            vector = np.zeros(n1d**ndim)
+            for j, vj in matched:
+                vector[j] = norm
+                handled += [j]
+
+            A1g_mat += [vector]
+        # print(handled)
+            # We could build a non-square projector P that we could apply to H, as in P @ H @ Pdagger
+            # Written that way, it'd take project to A1g states, dropping all non-A1g from the Hamiltonian.
+            # That'd make the Hamiltonian much much much smaller, and therefore (presumably) easier to diagonalize.
+            # However, it requires sparse-dense-sparse mat-mat-mat multiply.
+    return np.array(A1g_mat)
--- scripts/test_a1g.py
+++ scripts/test_a1g.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from luescher_nd.operators import get_A1g_operator as A1g
+from luescher_nd.operators import get_A1g_reducer as reducer
 import scipy.sparse as sp
 
 import matplotlib.pyplot as plt
@@ -9,22 +10,17 @@ import matplotlib.pyplot as plt
 ndim = 3
 n1d=6
 op = A1g(n1d, ndim)
-
-for i in range(n1d**ndim):
-    for j in range(n1d**ndim):
-        if np.abs(op[i,j] - op[j,i]) > 1e-7:
-            print(f"Not hermitian!  Problem with op[{i},{j}]≠op[{j},{i}]")
-            exit()
-print("Hermitian!")
-
 dense = op.todense()
+r = reducer(n1d, ndim)
 
 f = plt.figure()
-a = f.add_subplot(111)
-
-print(f"det = {np.linalg.det(dense)}")
-
+a = f.add_subplot(211)
+a.set_title(f"Nx = {n1d}, D={ndim}")
 a.imshow(dense)
+
+b = f.add_subplot(212)
+b.imshow(r)
+b.set_title(f"Shape = {r.shape}")
 
 plt.show()
 
