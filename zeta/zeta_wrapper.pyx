94c65904d7b52834e40008b3cbb621b368987613
Christopher Koerber <christopher@ckoerber.com>
2019-07-17 15:02:32 -0700

    Added python wrapper for spherical zeta

--- zeta/zeta_wrapper.pyx
+++ zeta/zeta_wrapper.pyx
@@ -5,25 +5,55 @@ from libcpp cimport bool
 import numpy as np
 
 
-cdef extern from "zeta.h":
-    cdef cppclass spherical:
-        spherical(unsigned int, unsigned int, bool) except +
-        bool filter(vector[unsigned int])
-        int degeneracy(vector[unsigned int])
-        double counterterm(double)
+cdef extern from "zeta_flat.h":
+    cdef cppclass SphericalZeta:
+        SphericalZeta(unsigned int, unsigned int, bool) except +
+        double operator()(const double)
 
+    cdef cppclass CartesianZeta:
+        CartesianZeta(unsigned int, unsigned int, bool) except +
+        double operator()(const double)
 
-cdef class SphericalDomain:
-    cdef spherical *c_spherical_ptr
+    cdef cppclass DispersionZeta:
+        DispersionZeta(
+            unsigned int,
+            unsigned int,
+            double,
+            unsigned int,
+            bool
+        ) except +
+        double operator()(const double)
+
+cdef class PySphericalZeta:
+    cdef SphericalZeta *c_ptr
 
     def __cinit__(self, unsigned int D, unsigned int N, bool improved=True):
-        self.c_spherical_ptr = new spherical(D, N, improved)
+        self.c_ptr = new SphericalZeta(D, N, improved)
 
-    def filter(self, vector[unsigned int] v):
-        return self.c_spherical_ptr.filter(v)
+    def __call__(self, const double x):
+        return self.c_ptr[0](x)
 
-    def degeneracy(self, vector[unsigned int] v):
-        return self.c_spherical_ptr.degeneracy(v)
+cdef class PyCartesianZeta:
+    cdef SphericalZeta *c_ptr
 
-    def counterterm(self, double x):
-        return self.c_spherical_ptr.counterterm(x)
+    def __cinit__(self, unsigned int D, unsigned int N, bool improved=True):
+        self.c_ptr = new CartesianZeta(D, N, improved)
+
+    def __call__(self, const double x):
+        return self.c_ptr[0](x)
+
+cdef class PyDispersionZeta:
+    cdef SphericalZeta *c_ptr
+
+    def __cinit__(
+        self,
+        unsigned int D,
+        unsigned int N,
+        double L,
+        unsigned int nstep=1,
+        bool improved=True
+    ):
+        self.c_ptr = new DispersionZeta(D, N, L, nstep, improved)
+
+    def __call__(self, const double x):
+        return self.c_ptr[0](x)
