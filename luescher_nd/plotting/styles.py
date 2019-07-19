dfa6321a8b0c7b0ae55c22b1f40d69685c5c37e7
Christopher Koerber <christopher@ckoerber.com>
2019-07-19 11:08:47 -0700

    Updated plot styles to match latex font

--- luescher_nd/plotting/styles.py
+++ luescher_nd/plotting/styles.py
@@ -5,29 +5,22 @@ import re
 import matplotlib
 import seaborn as sns
 
-
-RC = {
-    "font.family": "serif",
-    "font.serif": "Computer Modern Roman",
-    "font.sans-serif": "Computer Modern Sans serif",
-    "font.monospace": "Computer Modern Typewriter",
-    "text.usetex": False,
-}
-
 EXPORT_OPTIONS = {"bbox_inches": "tight"}
 LEGEND_STYLE = {"frameon": False, "loc": "center left", "bbox_to_anchor": (1.0, 0.5)}
 LINE_STYLE = {"ls": "-", "lw": 1.0}
 MARKERS = re.findall('``"([a-z]+)"``', matplotlib.markers.__doc__)
-MARKER_STYLE = {"ms": 1}
+MARKER_STYLE = {"ms": 3}
 
 
 def setup(n_colors: int = 5):
     """Sets up the font and colors
     """
     sns.set(
-        context="paper", style="ticks", font_scale=1, font="Computer Modern Roman", rc=RC
+        context="paper", style="ticks", font_scale=1.0, rc={"mathtext.fontset": "cm"}
     )
     sns.set_palette("cubehelix", n_colors=n_colors, desat=None, color_codes=False)
+
+    print(sns.axes_style())
 
 
 def finalize():
