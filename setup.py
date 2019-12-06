3978dcef580f1771b65b3fab32c44ec6d132aa52
Christopher Koerber <christopher@ckoerber.com>
2019-12-06 10:34:59 -0800

    Made it pip installable while downloading files

--- setup.py
+++ setup.py
@@ -6,9 +6,14 @@ from os import path
 
 import sys
 
+from urllib.request import urlretrieve
+
 from setuptools import setup
 from setuptools import Extension
 from setuptools import find_packages
+from setuptools.command.install import install as InstallCommand
+from setuptools.command.develop import develop as DevelopCommand
+from setuptools.command.egg_info import egg_info as EggInfoCommand
 
 from Cython.Build import cythonize
 
@@ -28,6 +33,75 @@ with open(path.join(THISDIR, "README.md"), encoding="utf-8", mode="r") as f:
 with open(path.join(THISDIR, "requirements.txt"), mode="r") as f:
     REQUIREMENTS = [el.strip() for el in f.read().split(",")]
 
+
+ZETA_EXTERNAL = path.join(THISDIR, "luescher_nd", "zeta", "extern", "zeta.cc")
+
+
+def download_extern():
+    """This function downloads `zeta.cc` and `zeta.h` from the repository
+
+    https://github.com/cjmorningstar10/TwoHadronsInBox
+
+    The files are needed for the three-dimensional zeta function
+    (`from luescher_nd.zeta.extern.pyzeta import zeta`).
+
+    If the files are not present, the library is still installed but the above import
+    will fail.
+    You either have to provide your own implementation or use this repo partially.
+    """
+    repository = "https://raw.githubusercontent.com/cjmorningstar10/TwoHadronsInBox/"
+    branch = "master"
+    file = "source/zeta.cc"
+    extensions = [".cc", ".h"]
+
+    info = """
+    This module depends on the files `zeta.cc` and `zeta.h`
+    from the repository
+
+    https://github.com/cjmorningstar10/TwoHadronsInBox
+
+    to computed the three-dimensional zeta function
+    (e.g., `from luescher_nd.zeta.extern.pyzeta import zeta`).
+
+    This script will place them in the right directory and compile them.
+    If the files are not present, the install will fail (you have to set
+    `CYTHONIZED_EXT=None` in this file and not import the above function.)
+    """
+    print(info)
+
+    for extension in extensions:
+        url = f"{repository}/{branch}/{file}".replace(".cc", extension)
+        file_path = ZETA_EXTERNAL.replace(".cc", extension)
+        urlretrieve(url, file_path)
+
+
+class CustomInstallCommand(InstallCommand):
+    def run(self):
+        if not path.exists(ZETA_EXTERNAL) or not path.exists(
+            ZETA_EXTERNAL.replace(".cc", ".h")
+        ):
+            download_extern()
+        InstallCommand.run(self)
+
+
+class CustomDevelopCommand(DevelopCommand):
+    def run(self):
+        if not path.exists(ZETA_EXTERNAL) or not path.exists(
+            ZETA_EXTERNAL.replace(".cc", ".h")
+        ):
+            download_extern()
+        DevelopCommand.run(self)
+
+
+class CustomEggInfoCommand(EggInfoCommand):
+    def run(self):
+        if not path.exists(ZETA_EXTERNAL) or not path.exists(
+            ZETA_EXTERNAL.replace(".cc", ".h")
+        ):
+            download_extern()
+        EggInfoCommand.run(self)
+
+
 EXTENSIONS = [
     Extension(
         path.join("luescher_nd", "zeta", "extern", "pyzeta").replace("/", "."),
@@ -45,6 +119,7 @@ EXTENSIONS = [
         extra_compile_args=["--std=c++11"],
     ),
 ]
+CYTHONIZED_EXT = cythonize(EXTENSIONS)
 
 setup(
     name="luescher_nd",
@@ -55,7 +130,12 @@ setup(
     author=__author__,
     packages=find_packages(exclude=["tests", "benchmarks"]),
     install_requires=REQUIREMENTS,
-    ext_modules=cythonize(EXTENSIONS),
+    ext_modules=CYTHONIZED_EXT,
     setup_requires=["cython", "numpy", "pytest-runner"],
     tests_require=["pytest"],
+    cmdclass={
+        "install": CustomInstallCommand,
+        "develop": CustomDevelopCommand,
+        "egg_info": CustomEggInfoCommand,
+    },
 )
